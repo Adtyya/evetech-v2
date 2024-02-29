b7bb4039f8f351443e8de2c4655bdacbe4fce589
aditya <aditdualapan@gmail.com>
2024-02-29 16:19:00 +0700

    adjust form input

--- app/service/mobile-app-development/page.js
+++ app/service/mobile-app-development/page.js
@@ -3,7 +3,7 @@ import SoftwareDevelopmentIndustry from "@/components/view/home/softwareDevelopm
 import Hero from "@/components/view/mobileappdevelopment/hero";
 import OurAgile from "@/components/view/mobileappdevelopment/ourAgile";
 import Portofolio from "@/components/view/mobileappdevelopment/portofolio";
-import ReadyToMeet from "@/components/view/mobileappdevelopment/readyToMeet";
+import ReadyToMeet from "@/components/view/webdevelopment/readyToMeet";
 
 export default function MobileAppDevelopment() {
   return (
--- app/service/web-app-development/page.js
+++ app/service/web-app-development/page.js
@@ -2,7 +2,7 @@ import FAQ from "@/components/view/global/FAQ";
 import SoftwareDevelopmentIndustry from "@/components/view/home/softwareDevelopmentIndustry";
 import Hero from "@/components/view/webappdevelopment/hero";
 import Portofolio from "@/components/view/webappdevelopment/portofolio";
-import ReadyToMeet from "@/components/view/webappdevelopment/readyToMeet";
+import ReadyToMeet from "@/components/view/webdevelopment/readyToMeet";
 
 export default function WebAppDevelopment() {
   return (
