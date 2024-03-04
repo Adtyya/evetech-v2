a7d1338a4506b7a9f8f340f3d6a1340ed1d2a837
aditya <aditdualapan@gmail.com>
2024-03-04 15:16:26 +0700

    add recruitment process

--- app/career/page.js
+++ app/career/page.js
@@ -1,11 +1,13 @@
 import Hero from "@/components/view/career/hero";
 import OurValues from "@/components/view/career/ourvalues";
+import RecruitmentProcess from "@/components/view/career/recruitmentprocess";
 
 export default function CareerPage() {
   return (
     <>
       <Hero></Hero>
       <OurValues></OurValues>
+      <RecruitmentProcess></RecruitmentProcess>
     </>
   );
 }
--- components/view/career/content/index.js
+++ components/view/career/content/index.js
@@ -24,3 +24,27 @@ export const ourValues = [
     image: "/images/career/values/4.png",
   },
 ];
+
+export const recruitmentprocess = [
+  {
+    id: 1,
+    title: "Curriculum Vitae (CV)",
+    subtitle:
+      "Buat CV menarik: cerminan prestasi, tunjukkan sebagai kandidat unggul dengan riwayat pendidikan dan pengalaman yang terstruktur.",
+    image: "/images/career/rec/1.png",
+  },
+  {
+    id: 2,
+    title: "Interview",
+    subtitle:
+      "Siap wawancara? Menangkan hati dengan pengetahuan dan jawaban mengesankan. Tunjukkan kepribadian unik dan keterampilan kolaboratif!",
+    image: "/images/career/rec/2.png",
+  },
+  {
+    id: 3,
+    title: "Offer",
+    subtitle:
+      "Optimalkan tawaran pekerjaan: terlibat aktif dalam proses, evaluasi cermat gaji dan tunjangan, pastikan kepuasan sebelum menerima, menuju karier sukses.",
+    image: "/images/career/rec/3.png",
+  },
+];
