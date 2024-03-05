84ceff472ac488c7efa0809c462a411967b8a517
aditya <aditdualapan@gmail.com>
2024-03-05 17:03:31 +0700

    add more padding container

--- components/box/container.jsx
+++ components/box/container.jsx
@@ -1,6 +1,9 @@
 export default function Container({ children, className = "", ...props }) {
   return (
-    <div className={`w-full max-w-7xl mx-auto px-3 ${className}`} {...props}>
+    <div
+      className={`w-full max-w-7xl mx-auto px-5 lg:px-3 ${className}`}
+      {...props}
+    >
       {children}
     </div>
   );
