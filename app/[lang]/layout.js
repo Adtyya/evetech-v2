7103b767e08fdfbbb62fc40394597596646ca125
aditya <aditdualapan@gmail.com>
2024-03-15 16:59:38 +0700

    update favicon

--- app/[lang]/layout.js
+++ app/[lang]/layout.js
@@ -11,9 +11,9 @@ export const metadata = {
   title: "Evetech: Software House Semarang & Jakarta",
   description:
     "Evetech Software House Semarang & Jakarta provides Custom Software & ERP Development, Web Development, Mobile App Development & Web Design in Indonesia and Malaysia.",
-  icons: {
-    icon: "./favicon.ico",
-  },
+  // icons: {
+  //   icon: "./favicon.ico",
+  // },
   verification: {
     google: "YeDPr9Oy1PD6fLQS5vg18h2wJwUmYjKT9WINun619ag",
   },
