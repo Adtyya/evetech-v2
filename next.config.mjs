5d0c3e91a6aeaf94e15fd0c5f58562d529bb08c1
aditya <aditdualapan@gmail.com>
2024-08-07 15:10:56 +0700

    fix path error

--- next.config.mjs
+++ next.config.mjs
@@ -36,29 +36,9 @@ const nextConfig = {
         permanent: true,
       },
       {
-        source: "/:lang/blog",
-        destination: "/:lang/blog",
-        permanent: true,
-      },
-      {
-        source: "/:lang/news-portal",
-        destination: "/:lang/services/news-web-app-development",
-        permanent: true,
-      },
-      {
-        source: "/:lang/portofolio",
-        destination: "/:lang/portfolio",
-        permanent: true,
-      },
-      {
         source:
           "/:lang/blog/top-7-web-application-development-tools-empowering-developers-in-2023",
         destination: "/:lang/blog",
-        permanent: true,
-      },
-      {
-        source: "/:lang/news-portal",
-        destination: "/:lang/services/news-web-app-development",
         permanent: true,
       },
       {
@@ -73,45 +53,15 @@ const nextConfig = {
         permanent: true,
       },
       {
-        source: "/:lang/web-app-development",
-        destination: "/:lang/services/web-app-development",
-        permanent: true,
-      },
-      {
-        source: "/:lang/services/news-web-development",
-        destination: "/:lang/services/news-web-app-development",
-        permanent: true,
-      },
-      {
-        source: "/:lang/web-development",
-        destination: "/:lang/services/web-app-development",
-        permanent: true,
-      },
-      {
         source:
           "/:lang/blog/recognize-brand-archetypes-for-your-business-development",
         destination: "/:lang/blog",
         permanent: true,
       },
       {
-        source: "/:lang/news-portal",
-        destination: "/:lang/services/news-web-app-development",
-        permanent: true,
-      },
-      {
         source:
           "/:lang/blog/why-businesses-need-social-media-specialists-for-success",
         destination: "/:lang/blog",
-        permanent: true,
-      },
-      {
-        source: "/:lang/mobile-app-development",
-        destination: "/:lang/services/mobile-app-development",
-        permanent: true,
-      },
-      {
-        source: "/:lang/news-portal/whatsapp",
-        destination: "/:lang/services/news-web-app-development",
         permanent: true,
       },
       {
@@ -143,11 +93,6 @@ const nextConfig = {
         permanent: true,
       },
       {
-        source: "/:lang/portofolio",
-        destination: "/:lang/portfolio",
-        permanent: true,
-      },
-      {
         source:
           "/:lang/blog/autopilot-business-a-guide-to-maximize-your-business",
         destination: "/:lang/blog",
@@ -168,16 +113,6 @@ const nextConfig = {
         source:
           "/:lang/blog/4-success-entrepreneurs-in-indonesia-a-glimpse-into-visionary-leaders",
         destination: "/:lang/blog",
-        permanent: true,
-      },
-      {
-        source: "/:lang/:lang/about-us",
-        destination: "/:lang/about-us",
-        permanent: true,
-      },
-      {
-        source: "/:lang/news-portal/whatsapp",
-        destination: "/:lang/services/news-web-app-development",
         permanent: true,
       },
       {
