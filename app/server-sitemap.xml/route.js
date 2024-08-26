4f65003488dc1f53ddd2538b634f1feda4425bf9
sanusievetech <sanusi@evetechsolution.com>
2024-08-26 19:11:09 +0700

    fix meta blog

--- app/server-sitemap.xml/route.js
+++ app/server-sitemap.xml/route.js
@@ -2,37 +2,22 @@ import api from "@/utils/axios";
 import { getServerSideSitemap } from "next-sitemap";
 
 export async function GET(req) {
-  // const urls = await api.get("/posts?sort=createdAt:desc");
-  // return getServerSideSitemap(
-  //   urls.data?.data?.map((item) => {
-  //     return {
-  //       loc: `https://evetechsolution.com/blog/read/${item?.attributes?.slug}`,
-  //       lastmod: item?.attributes?.updatedAt,
-  //       priority: 1,
-  //       changefreq: "daily",
-  //     };
-  //   })
-  // );
-  
   const allPosts = [];
   let page = 1;
   let pageSize = 100; // Adjust this based on your API's max page size
   let hasMore = true;
 
   while (hasMore) {
-    const response = await api.get(`/posts`, {
+    const response = await api.get(`/blog/available`, {
       params: {
-        sort: 'createdAt:desc',
-        pagination: {
-          page: page,
-          pageSize: pageSize
-        }
+        page: page,
+        perPage: pageSize
       }
     });
-    
+
     const data = response.data?.data;
     allPosts.push(...data);
-    
+
     // Check if there are more pages
     hasMore = data.length === pageSize;
     page++;
@@ -41,8 +26,8 @@ export async function GET(req) {
   return getServerSideSitemap(
     allPosts.map((item) => {
       return {
-        loc: `https://evetechsolution.com/blog/read/${item?.attributes?.slug}`,
-        lastmod: item?.attributes?.updatedAt,
+        loc: `https://evetechsolution.com/blog/read/${item?.slug}`,
+        lastmod: item?.date,
         priority: 1,
         changefreq: "daily",
       };
