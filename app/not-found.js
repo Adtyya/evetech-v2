6cf72cadc33c8b3106d7dcd7780682d83d06ca18
sanusievetech <sanusi@evetechsolution.com>
2024-09-06 18:12:51 +0700

    fix bug

--- app/not-found.js
+++ app/not-found.js
@@ -2,10 +2,11 @@ import { Inter } from "next/font/google";
 import NavbarEvetech from "@/components/navbar/navbarNotFound";
 import Footer from "@/components/footer/footer";
 import NextTopLoader from "nextjs-toploader";
-import { NextIntlClientProvider, useMessages } from "next-intl";
+import { NextIntlClientProvider, useMessages, useLocale } from "next-intl";
 import Image from "next/image";
 import Container from "@/components/box/container";
 import Heading from "@/components/text/heading";
+import { unstable_setRequestLocale } from "next-intl/server";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,14 +22,26 @@ export function generateStaticParams() {
   return locales.map((locale) => ({ lang: locale }));
 }
 
-export default function NotFound() {
+export async function generateMetadata({ params }) {
+  unstable_setRequestLocale(params.lang); // Atur lokalitas di server
+  return {
+    title: "Page Not Found",
+    description: "The page you're looking for does not exist.",
+  };
+}
+
+export default function NotFound({ params }) {
   const messages = useMessages();
+  const locale = useLocale(); // Dapatkan lokalitas dari hook
+
+  // Jika params.lang ada, gunakan itu; jika tidak, gunakan lokalitas dari hook
+  const lang = params?.lang || locale || "en";
 
   return (
-    <html lang={"en"}>
+    <html lang={lang}>
       <body className={inter.className}>
         <NextTopLoader color="#3A85FF" zIndex={2000} />
-        <NextIntlClientProvider messages={messages} locale={"en"}>
+        <NextIntlClientProvider messages={messages} locale={lang}>
           <NavbarEvetech />
           <Container>
             <div className="flex flex-col w-full h-[85vh] items-center justify-center">
